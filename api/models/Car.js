9226913d55820a6b9d0fc7fc107efb5d89655043
Luiz Carraro <lacarraro@gmail.com>
2016-05-18 16:42:34 -0300

    add model Car and relates it to User

--- api/models/Car.js
+++ api/models/Car.js
@@ -1,5 +1,5 @@
 /**
- * User.js
+ * Car.js
  *
  * @description :: TODO: You might write a short summary of how this model works and what it represents here.
  * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
@@ -8,9 +8,8 @@
 module.exports = {
 
   attributes: {
-  	cars: {
-  		collection: 'car',
-  		via: 'owner'
+  	owner: {
+  		model: 'user'
   	}
   }
 };
